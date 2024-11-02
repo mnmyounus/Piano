688e93bc85f2c96a20bec3577f931bd178e7a8bc
mnmyounus <mnmyounus@gmail.com>
2024-11-02 13:32:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ volumeSlider = document.querySelector(".volume-slider input"),
 keysCheckbox = document.querySelector(".keys-checkbox input");
 
 let allKeys = [],
-audio = new Audio(`tunes/a.wav`); // by default, audio src is "a" tune
+audio = new Audio(`a.wav`); // by default, audio src is "a" tune
 
 const playTune = (key) => {
-    audio.src = `tunes/${key}.wav`; // passing audio src based on key pressed 
+    audio.src = `${key}.wav`; // passing audio src based on key pressed 
     audio.play(); // playing audio
 
     const clickedKey = document.querySelector(`[data-key="${key}"]`); // getting clicked key element
